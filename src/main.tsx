0376ace91f2512d8d1dd7c83b13bfbd3a1fd1322
luistc92 <luis_tc92@hotmail.com>
2025-06-12 17:04:41 -0600

    added clerk

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ClerkProvider } from '@clerk/clerk-react'
+
+
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ConvexProvider client={convex}>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+        <App />
+      </ClerkProvider>
+    </ConvexProvider>
   </StrictMode>,
 )
